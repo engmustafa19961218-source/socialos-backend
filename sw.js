a8ed3615b00fbe58febaa8a4251369f49216150e
Your Name <engmustafa19961218@gmail.com>
2026-06-13 02:55:22 +0300

    Fix missing functions v60b

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'sos-v59';
+const V = 'sos-v60b';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))).then(() => self.clients.claim()));
