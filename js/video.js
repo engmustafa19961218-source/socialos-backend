8f5354cd9a92e0e3a247b9e6fa6bed07d7ee7fc5
Your Name <engmustafa19961218@gmail.com>
2026-06-15 04:01:57 +0300

    Fix video polling status check

--- js/video.js
+++ js/video.js
@@ -172,7 +172,7 @@ function pollVideoStatus(renderId, resultEl) {
 
     const d = await api(`/api/video/status/${renderId}`);
 
-    if (d.ready && d.url) {
+    if ((d.ready || d.status === 'done') && d.url) {
       clearInterval(_pollInterval);
       showVideoReady(resultEl, d.url);
       toast('🎬 الفيديو جاهز!');
